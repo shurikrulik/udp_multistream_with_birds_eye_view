22fda2649551be357b3afc607cf41c4494c3908e
Aleksandr <alxsave@gmail.com>
2018-11-12 16:00:03 +0300

    First success with async call

--- mt.cpp
+++ mt.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
-#include<thread>
-#include<mutex>
-#include<chrono>
-#include<vector>
-#include<stdexcept>
-#include<string>
-#define sleepTime 0.005s
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+#include <stdexcept>
+#include <string>
+#include <future>
+
+#define sleepTime 0.01s
 
 using namespace std;
 using namespace std::chrono_literals;
@@ -18,33 +20,44 @@ std::string mutation(string message, int threadNumber)
 	return message;
 }
 
-void printer(string message, int threadNumber)
+std::string printer(string message, int threadNumber)
 {
 	int iterator = 0;
-	while(1)
-	{
+	std::string newMessage;
+	
 		auto timerStart = std::chrono::high_resolution_clock::now();
-		std::lock_guard<std::mutex> lock(mutexSync);
-		string newMessage = mutation(message, threadNumber);
-		cout<<newMessage<<endl;
-		mutexSync.unlock();
-		std::this_thread::sleep_for(sleepTime);
-    		auto timerEnd = std::chrono::high_resolution_clock::now();
-	}
+		if(mutexSync.try_lock())
+		{
+			newMessage = mutation(message, threadNumber);
+			//cout<<newMessage<<endl;
+			
+			mutexSync.unlock();
+			std::this_thread::sleep_for(sleepTime);
+	    		auto timerEnd = std::chrono::high_resolution_clock::now();
+		}
+	return newMessage;
+	
 }
 
 int main(int argc, char* argv[])
 {
 	std::vector<std::thread> threads;
-	for(int threadNumber = 0; threadNumber < stoi(argv[1]); threadNumber++)
-	{
-		std::string message;
-		std::thread _thread(printer, message, threadNumber);
-		threads.emplace_back(std::move(_thread));
-	}
-	for(auto &_thread : threads)
+	std::vector<std::string> resultString;
+	while(1){
+		for(int threadNumber = 0; threadNumber < stoi(argv[1]); threadNumber++)
+		{
+			std::string message;
+			//std::thread _thread(printer, message, threadNumber);
+			std::future<std::string> resultString = std::async(std::launch::async, printer, message, threadNumber); 
+			//threads.emplace_back(std::move(_thread));
+			auto stringForPrint = resultString.get();
+			cout<<stringForPrint<<endl;
+		}
+
+	/*for(auto &_thread : threads)
 	{
 		_thread.join();
+	}*/
 	}
 	return 0;
 }
