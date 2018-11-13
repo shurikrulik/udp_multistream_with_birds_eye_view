d790314718085be9e956eea79b1e5dc69eee9a08
Aleksandr <alxsave@gmail.com>
2018-11-13 15:48:39 +0300

    worked udp non async, but through function

--- parts/Server.cpp
+++ parts/Server.cpp
@@ -9,7 +9,6 @@
 #include <string>
 #include <stdio.h>
 #include <stdarg.h>
-#include <future>
 
 #include "opencv2/opencv.hpp"
 #include "config.h"
@@ -23,51 +22,44 @@ using namespace std::chrono_literals;
 
 std::mutex mutexSync;
 
-Mat udp_receive(unsigned short servPort)
+void udp_receive(unsigned short servPort)
 {
 	UDPSocket sock(servPort);
-	Mat frame;
+
         char buffer[BUF_LEN]; // Buffer for echo string
         int recvMsgSize; // Size of received message
         string sourceAddress; // Address of datagram source
         unsigned short sourcePort; // Port of datagram source
-	
+
         clock_t last_cycle = clock();
 
-        while (1) 
-         {
-	    if(mutexSync.try_lock())
-	    {
-		    // Block until receive message from a client
-		    do {
-		        recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
-		    } while (recvMsgSize > sizeof(int));
-		    int total_pack = ((int * ) buffer)[0];
+        while (1) {
+            // Block until receive message from a client
+            do {
+                recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
+            } while (recvMsgSize > sizeof(int));
+            int total_pack = ((int * ) buffer)[0];
 
-		    cout << "expecting length of packs:" << total_pack << endl;
-		    char * longbuf = new char[PACK_SIZE * total_pack];
-		    for (int i = 0; i < total_pack; i++) {
-		        recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
-		        if (recvMsgSize != PACK_SIZE) {
-		            cerr << "Received unexpected size pack:" << recvMsgSize << endl;
-		            continue;
-		        }
-		        memcpy( & longbuf[i * PACK_SIZE], buffer, PACK_SIZE);
-		    }
+            cout << "expecting length of packs:" << total_pack << endl;
+            char * longbuf = new char[PACK_SIZE * total_pack];
+            for (int i = 0; i < total_pack; i++) {
+                recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
+                if (recvMsgSize != PACK_SIZE) {
+                    cerr << "Received unexpected size pack:" << recvMsgSize << endl;
+                    continue;
+                }
+                memcpy( & longbuf[i * PACK_SIZE], buffer, PACK_SIZE);
+            }
 
-		    cout << "Received packet from " << sourceAddress << ":" << sourcePort << endl;
-	 
-		    Mat rawData = Mat(1, PACK_SIZE * total_pack, CV_8UC1, longbuf);
-		    frame = imdecode(rawData, CV_LOAD_IMAGE_COLOR);
-		    if (frame.size().width == 0) {
-		        cerr << "decode failure!" << endl;
-		        continue;
-		    }
-		mutexSync.unlock();
-	        std::this_thread::sleep_for(sleepTime);
-	    }
-	    return frame;
-            /*imshow("recv", frame);
+            cout << "Received packet from " << sourceAddress << ":" << sourcePort << endl;
+ 
+            Mat rawData = Mat(1, PACK_SIZE * total_pack, CV_8UC1, longbuf);
+            Mat frame = imdecode(rawData, CV_LOAD_IMAGE_COLOR);
+            if (frame.size().width == 0) {
+                cerr << "decode failure!" << endl;
+                continue;
+            }
+            imshow("recv", frame);
             free(longbuf);
 
             waitKey(1);
@@ -76,8 +68,8 @@ Mat udp_receive(unsigned short servPort)
             cout << "\teffective FPS:" << (1 / duration) << " \tkbps:" << (PACK_SIZE * total_pack / duration / 1024 * 8) << endl;
 
             cout << next_cycle - last_cycle;
-            last_cycle = next_cycle;*/
-	  }
+            last_cycle = next_cycle;
+}
 }
 
 int main(int argc, char * argv[]) 
@@ -93,18 +85,17 @@ int main(int argc, char * argv[])
     unsigned short servPort = atoi(argv[1]); // First arg:  local port
 
     namedWindow("recv", CV_WINDOW_AUTOSIZE);
-    
-	    try 
-	    {
-	    	std::future<Mat> frame = std::async(std::launch::async, udp_receive, servPort);
-		Mat receivedFrame = frame.get();
-            	imshow("recv", receivedFrame);
-	    }
-	    catch (SocketException & e) 
+    while(1)
+    {
+	    try {
+		    udp_receive(servPort);
+		}
+	     catch (SocketException & e) 
             {
 		cerr << e.what() << endl;
 		exit(1);
 	    }
-    
+    }
+
     return 0;
 }
